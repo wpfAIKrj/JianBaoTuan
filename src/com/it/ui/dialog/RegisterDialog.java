d3fa571e950aa7fe8449b2ac524d97454d43f0f1
guyif3 <guyif3@sina.com>
2015-04-25 21:03:54 +0800

    更新

---
+++ src/com/it/ui/dialog/RegisterDialog.java
@@ -0,0 +1,25 @@
+package com.it.ui.dialog;
+
+import com.it.R;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Window;
+
+public class RegisterDialog extends Dialog {
+
+	public RegisterDialog(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.dialog_register);
+	}
+
+}
