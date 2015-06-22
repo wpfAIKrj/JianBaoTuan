b88ac554acce6a6dc780386d79423440049f5438
Administrator <Administrator@192.168.56.1>
2015-06-22 16:31:25 +0800

    修改我收藏的鉴定

--- src/com/yingluo/Appraiser/view/viewholder/ViewTreasure.java
+++ src/com/yingluo/Appraiser/view/viewholder/ViewTreasure.java
@@ -7,6 +7,7 @@ import com.lidroid.xutils.view.annotation.ViewInject;
 import com.yingluo.Appraiser.R;
 import com.yingluo.Appraiser.bean.TreasureEntity;
 import com.yingluo.Appraiser.utils.BitmapsUtils;
+import com.yingluo.Appraiser.utils.DensityUtil;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView.ViewHolder;
@@ -67,24 +68,37 @@ public class ViewTreasure extends ViewHolder {
 		tv_status.setText(item.status == 1 ? "已鉴定" : "未鉴定");
 		layout_kind.removeAllViews();
 		List<com.yingluo.Appraiser.bean.TreasureEntity.Kind> kinds = item.kinds;
+		
 		if (kinds != null) {
-			LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT,
-					LayoutParams.MATCH_PARENT);
-			params.setMargins(0, 0, 20, 0);
-			params.gravity = Gravity.CENTER;
+			LayoutParams params =null;
 
+			int size=0;
+			int width=0;
+			int onewidth=(int) mContext.getResources().getDimension(R.dimen.x60);
+			int height=(int) mContext.getResources().getDimension(R.dimen.y30);
+			int textsize=DensityUtil.px2sp( mContext,  mContext.getResources().getDimension(R.dimen.x20));
+			
 			int len = kinds.size();
 			for (int i = 0; i < len; i++) {
+				size=kinds.get(i).name.length();
+				if(size>8){
+					size=7;
+				}
+				width=(size/2)*onewidth+(size%2)*onewidth;
+				params=new LayoutParams(width,
+							height);
+				params.setMargins(0, 0, 20, 0);
 				TextView view = new TextView(mContext);
-				view.setTextSize(12f);
+				view.setTextSize(textsize);
 				view.setTextColor(mContext.getResources().getColor(R.color.wite));
 				view.setBackgroundColor(mContext.getResources().getColor(
 						R.color.number_color));
-				view.setPadding(10, 0, 10, 0);
+//				view.setPadding(10, 0, 10, 0);
+				view.setGravity(Gravity.CENTER);
 				view.setText(kinds.get(i).name);
 				view.setTag(kinds.get(i).id);
-
-				layout_kind.addView(view, params);
+				view.setLayoutParams(params);
+				layout_kind.addView(view);
 			}
 
 		}
