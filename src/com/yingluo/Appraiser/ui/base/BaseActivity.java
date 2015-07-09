0936cf9562e9ce8577c67a0783b330f62f02f79c
Administrator <Administrator@192.168.56.1>
2015-07-09 20:26:45 +0800

    当输入框失去焦点的时候，键盘自动消失

--- src/com/yingluo/Appraiser/ui/base/BaseActivity.java
+++ src/com/yingluo/Appraiser/ui/base/BaseActivity.java
@@ -3,16 +3,21 @@ package com.yingluo.Appraiser.ui.base;
 
 import com.yingluo.Appraiser.R;
 import com.yingluo.Appraiser.utils.ActivityTaskManager;
+import com.yingluo.Appraiser.utils.HelpUtils;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 
 public class BaseActivity extends Activity {
 	//protected SystemBarTintManager mTintManager;
@@ -46,4 +51,24 @@ public class BaseActivity extends Activity {
 	        }
 	        win.setAttributes(winParams);
 	}
+	
+	
+	/**
+     * 隐藏软键盘
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        // TODO Auto-generated method stub
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+ 
+            // 获得当前得到焦点的View，一般情况下就是EditText（特殊情况就是轨迹求或者实体案件会移动焦点）
+            View v = getCurrentFocus();
+ 
+            if (HelpUtils.isShouldHideInput(v, ev)) {
+                InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                HelpUtils.hideSoftInput(v.getWindowToken(), im);
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
 }
