1930a31124acb759fec45c7239c8d68a67c3de5e
WangYaLi <18516894379@163.com>
2015-07-31 10:33:16 +0800

    修改了聊天的相关问题

---
+++ src/com/yingluo/Appraiser/ui/activity/IMListActivity.java
@@ -0,0 +1,62 @@
+package com.yingluo.Appraiser.ui.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yingluo.Appraiser.R;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+
+/**
+ * 聊天页面
+ * 
+ * @author Administrator
+ *
+ */
+public class IMListActivity extends FragmentActivity {
+
+	@ViewInject(R.id.home_title)
+	private TextView title;
+
+	@ViewInject(R.id.button_delect)
+	private ImageView iv_delete;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_imlistview);
+		ViewUtils.inject(this);
+		initView();
+
+	}
+
+	protected void initView() {
+		// TODO Auto-generated method stub
+
+		title.setText("聊天");
+		iv_delete.setVisibility(View.GONE);
+
+	}
+
+	@OnClick({ R.id.button_category })
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.button_category:// 跳转到
+			setResult(RESULT_CANCELED, getIntent());
+			finish();
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
