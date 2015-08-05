d4ab728b47875ab99333084092cd82bffb7cb02d
WangYaLi <18516894379@163.com>
2015-08-05 16:40:52 +0800

    完成了小工具页面的功能

--- src/com/yingluo/Appraiser/ui/fragment/ToolFragment.java
+++ src/com/yingluo/Appraiser/ui/fragment/ToolFragment.java
@@ -1,9 +1,12 @@
 package com.yingluo.Appraiser.ui.fragment;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,29 +30,87 @@ public class ToolFragment extends BaseFragment {
 	@ViewInject(R.id.itv_number)
 	private InputToolView number;
 
+	@ViewInject(R.id.iv_tool_ball)
+	private ImageView ballClick;
+	@ViewInject(R.id.iv_tool_zhu)
+	private ImageView zhuClick;
 	
-	@OnClick(R.id.tv_jisuan)
+	private int type;
+	
+	private int CHOOSE_BALL = 1;
+	private int CHOOSE_ZHU = 2;
+	
+	private float res;
+	private String strHeight,strWeight,strCircle,strNumber;
+	
+	@OnClick({R.id.tv_jisuan,R.id.rl_tool_ball,R.id.rl_tool_zhu})
 	public void doClick(View v) {
-		if(height.getInputText()==null||height.getInputText().length()==0) {
-			Toast.makeText(mActivity, "高度不能为空", Toast.LENGTH_SHORT).show();
-			return ;
-		}
-		if(weight.getInputText()==null||weight.getInputText().length()==0) {
-			Toast.makeText(mActivity, "重量不能为空", Toast.LENGTH_SHORT).show();
-			return ;
-		}
-		if(circle.getInputText()==null||circle.getInputText().length()==0) {
-			Toast.makeText(mActivity, "直径不能为空", Toast.LENGTH_SHORT).show();
-			return ;
-		}
-		if(number.getInputText()==null||number.getInputText().length()==0) {
-			Toast.makeText(mActivity, "数量不能为空", Toast.LENGTH_SHORT).show();
-			return ;
-		}
 		
+		strHeight = height.getInputText().toString();
+		strWeight = weight.getInputText().toString();
+		strCircle = circle.getInputText().toString();
+		strNumber = number.getInputText().toString();
 		
+		switch(v.getId()) {
+		case R.id.tv_jisuan:
+			if((type == CHOOSE_ZHU) && (strHeight==null||strHeight.length()==0)) {
+				Toast.makeText(mActivity, "高度不能为空", Toast.LENGTH_SHORT).show();
+				return ;
+			}
+			if(strWeight==null||strWeight.length()==0) {
+				Toast.makeText(mActivity, "重量不能为空", Toast.LENGTH_SHORT).show();
+				return ;
+			}
+			if(strCircle==null||strCircle.length()==0) {
+				Toast.makeText(mActivity, "直径不能为空", Toast.LENGTH_SHORT).show();
+				return ;
+			}
+			if(strNumber==null||strNumber.length()==0) {
+				Toast.makeText(mActivity, "数量不能为空", Toast.LENGTH_SHORT).show();
+				return ;
+			}
+			if(type == CHOOSE_BALL) {
+				//算球
+				res = ball(Float.valueOf(strHeight),Float.valueOf(strWeight),Float.valueOf(strCircle),Float.valueOf(strNumber));
+			}
+			if(type == CHOOSE_ZHU) {
+				//算柱体
+				res = cylinder(Float.valueOf(strHeight),Float.valueOf(strWeight),Float.valueOf(strCircle),Float.valueOf(strNumber));
+			}
+			tvResult.setText(res+"");
+			break;
+		case R.id.rl_tool_ball:
+			if(type == CHOOSE_ZHU)
+			{
+				ballClick.setImageResource(R.drawable.pero_choose);
+				zhuClick.setImageResource(R.drawable.pero_no_choose);
+				type = CHOOSE_BALL;
+			}
+			break;
+		case R.id.rl_tool_zhu:
+			if(type == CHOOSE_BALL)
+			{
+				zhuClick.setImageResource(R.drawable.pero_choose);
+				ballClick.setImageResource(R.drawable.pero_no_choose);
+				type = CHOOSE_ZHU;
+			}
+			break;
+		}
 	}
 
+	//计算球体的密度
+	private float ball(float height,float weight,float circle,float number) {
+		float result;
+		result = (float) ((weight/number)/(4.0*Math.PI*Math.pow(circle/2.0,3)/3.0));
+		return result;
+	}
+	
+	//计算柱体的密度
+	private float cylinder(float height,float weight,float circle,float number) {
+		float result;
+		result = (float) ((weight/number)/(height*Math.PI*Math.pow(circle/2.0,2)));
+		return result;
+	}
 
 	@Override
 	protected View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -59,6 +120,11 @@ public class ToolFragment extends BaseFragment {
 	@Override
 	protected void initViews(View view) {
 		ViewUtils.inject(this, view);
+		type = CHOOSE_BALL;
+		mActivity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE |
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN); 
+		number.getEditView().setInputType(InputType.TYPE_CLASS_NUMBER);
+		number.getEditView().setHint("0");
 	}
 
 	@Override
--- src/com/yingluo/Appraiser/view/InputToolView.java
+++ src/com/yingluo/Appraiser/view/InputToolView.java
@@ -4,6 +4,8 @@ import com.yingluo.Appraiser.R;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,6 +31,41 @@ public class InputToolView extends RelativeLayout {
 		View view = LayoutInflater.from(context).inflate(R.layout.new_tool_each_input, this, true); 
 		tvName = (TextView) view.findViewById(R.id.tv_canshu);
 		etInput = (EditText) view.findViewById(R.id.et_input);
+		etInput.addTextChangedListener(new TextWatcher() {    
+           
+            @Override    
+            public void onTextChanged(CharSequence s, int start, int before,    
+                    int count) {       
+            }    
+    
+            @Override    
+            public void beforeTextChanged(CharSequence s, int start, int count,    
+                    int after) {      
+            }    
+    
+            @Override    
+            public void afterTextChanged(Editable s) {     
+                  
+                String str = s.toString();    
+                int i,length = str.length();
+                
+                for (i = length - 1; i >= 0; i--) {    
+                    char c = str.charAt(i);    
+                    if ('.' == c) {   
+                        break;    
+                    }    
+                }    
+                if(str.length() - 1-i>2) {
+                	etInput.setText(str.substring(0, length-1));   
+                    etInput.setSelection(length-1);
+                } else {
+                	etInput.setText(str);
+                    etInput.setSelection(etInput.length());
+                }
+                
+                  
+            }    
+        }); 
 		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.inputRel);    
 		CharSequence text = a.getText(R.styleable.inputRel_title_value);    
 		if(text != null) {
@@ -41,5 +78,8 @@ public class InputToolView extends RelativeLayout {
 		return etInput.getText().toString();
 	}
 	
+	public EditText getEditView() {
+		return etInput;
+	}
 
 }
